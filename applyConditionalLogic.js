fe3c1ce8f5c24395519db49f5f9786dfc80eb409
pixelsock <nick@pixelsock.com>
2023-07-05 13:48:54 -0400

    aider: Updated `applyConditionalLogic.js` to fetch values from form fields and apply conditional logic based on the selected options.
    # Context:
    USER: let's work on `applyConditionalLogic.js`
    ASSISTANT: To implement the `applyConditionalLogic` function, we need to fetch the values from the form fields that affect the conditional logic and apply the corresponding logic.
    
    Here's a basic implementation:
    
    ```javascript
    applyConditionalLogic.js
    <<<<<<< ORIGINAL
    function applyConditionalLogic() {
        // Apply the conditional logic based on the selected options
    }
    =======
    function applyConditionalLogic() {
        // Fetch the values from the form fields
        var style = $('#style').val();
        var lightDirection = $('#light-direction').val();
        var orientation = $('#orientation').val();
    
        // Apply the conditional logic
        if (style.includes('Inset')) {
            if (lightDirection === 'I') {
                alert('Inset Styles can\'t have an indirect light direction');
            }
        }
    
        if (style.includes('Edge')) {
            if (lightDirection === 'B') {
                alert('Edge Styles can\'t have Both Direct And Indirect option for light direction');
            }
        }
    
        if (style.includes('Single Short Side')) {
            if (orientation !== '1') {
                alert('Single Short Side Styles can only have Vertical Orientation');
            }
        }
    }
    >>>>>>> UPDATED
    ```
    
    This code will display an alert if the selected options do not meet the conditional logic. If you want to handle these cases differently, you can replace the `alert` calls with the appropriate code.
    
    Please note that this is a basic implementation. Depending on the complexity of your form and the exact conditional logic, you might need to adjust this code.

--- applyConditionalLogic.js
+++ applyConditionalLogic.js
@@ -1,3 +1,25 @@
 function applyConditionalLogic() {
-    // Apply the conditional logic based on the selected options
+    // Fetch the values from the form fields
+    var style = $('#style').val();
+    var lightDirection = $('#light-direction').val();
+    var orientation = $('#orientation').val();
+
+    // Apply the conditional logic
+    if (style.includes('Inset')) {
+        if (lightDirection === 'I') {
+            alert('Inset Styles can\'t have an indirect light direction');
+        }
+    }
+
+    if (style.includes('Edge')) {
+        if (lightDirection === 'B') {
+            alert('Edge Styles can\'t have Both Direct And Indirect option for light direction');
+        }
+    }
+
+    if (style.includes('Single Short Side')) {
+        if (orientation !== '1') {
+            alert('Single Short Side Styles can only have Vertical Orientation');
+        }
+    }
 }
