9443b7ce758660cfa624075c0a7700029d5cf759
pixelsock <nick@pixelsock.com>
2023-07-05 13:25:56 -0400

    aider: Simplified the `generateSKU.js` function to append the value of each form field to the SKU.
    # Context:
    USER: now let's change the `generateSKU.js`
    ASSISTANT: Sure, we can simplify the `generateSKU.js` function since we've updated the HTML values to be SKU-friendly. Here's how you can update the `generateSKU.js` file:
    
    ```javascript
    generateSKU.js
    <<<<<<< ORIGINAL
    function generateSKU() {
        var sku = '';
        var styleCodes = {
            'full-frame-inset': '01',
            'full-frame-edge': '02',
            'double-long-side-inset': '03',
            'double-long-side-edge': '04',
            'no-frost': '05',
            'double-short-side-edge': '06',
            'double-short-side-inset': '07',
            'single-long-side-edge': '08',
            'single-short-side-edge': '09',
            'single-short-side-inset': '10',
            'round-full-frame-edge': '11',
            'round-no-frost': '12'
        };
        var orientationCodes = {
            'vertical': '1',
            'horizontal': '2'
        };
        $('#product-configurator select, #product-configurator input').each(function() {
            if (this.id === 'style') {
                sku += styleCodes[$(this).val()];
            } else if (this.id === 'size') {
                sku += $(this).val().replace('x', '');
            } else if (this.id === 'orientation') {
                sku += orientationCodes[$(this).val()];
            } else {
                sku += $(this).val();
            }
        });
        $('#product-details').text('SKU: ' + sku);
    }
    =======
    function generateSKU() {
        var sku = '';
        $('#product-configurator select, #product-configurator input').each(function() {
            sku += $(this).val();
        });
        $('#product-details').text('SKU: ' + sku);
    }
    >>>>>>> UPDATED
    ```
    
    This will simply append the value of each form field to the SKU.

--- generateSKU.js
+++ generateSKU.js
@@ -1,33 +1,7 @@
 function generateSKU() {
     var sku = '';
-    var styleCodes = {
-        'full-frame-inset': '01',
-        'full-frame-edge': '02',
-        'double-long-side-inset': '03',
-        'double-long-side-edge': '04',
-        'no-frost': '05',
-        'double-short-side-edge': '06',
-        'double-short-side-inset': '07',
-        'single-long-side-edge': '08',
-        'single-short-side-edge': '09',
-        'single-short-side-inset': '10',
-        'round-full-frame-edge': '11',
-        'round-no-frost': '12'
-    };
-    var orientationCodes = {
-        'vertical': '1',
-        'horizontal': '2'
-    };
     $('#product-configurator select, #product-configurator input').each(function() {
-        if (this.id === 'style') {
-            sku += styleCodes[$(this).val()];
-        } else if (this.id === 'size') {
-            sku += $(this).val().replace('x', '');
-        } else if (this.id === 'orientation') {
-            sku += orientationCodes[$(this).val()];
-        } else {
-            sku += $(this).val();
-        }
+        sku += $(this).val();
     });
     $('#product-details').text('SKU: ' + sku);
 }
