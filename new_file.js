c6f309349c8a5b377764c82ac93075f43187e1f2
pixelsock <nick@pixelsock.com>
2023-07-05 12:52:31 -0400

    aider: Add the rest of the style codes to the `generateSKU` function.
    # Context:
    USER: add the rest of the style codes
    USER: add the rest of the style codes
    USER: add the rest of the style codes
    ASSISTANT: Sure, let's add the rest of the style codes to the `generateSKU` function in the JavaScript file.
    
    ```javascript
    new_file.js
    <<<<<<< ORIGINAL
    function generateSKU() {
        var sku = '';
        var styleCodes = {
            'full-frame-inset': '01',
            'full-frame-edge': '02',
            // Add the rest of the style codes here
        };
        $('#product-configurator select, #product-configurator input').each(function() {
            if (this.id === 'style') {
                sku += styleCodes[$(this).val()];
            } else {
                sku += $(this).val();
            }
        });
        $('#product-details').text('SKU: ' + sku);
    }
    =======
    function generateSKU() {
        var sku = '';
        var styleCodes = {
            'full-frame-inset': '01',
            'full-frame-edge': '02',
            'double-long-side-inset': '03',
            'double-long-side-edge': '04',
            'no-frost': '05',
            'double-short-side-edge': '06',
            'double-short-side-inset': '07',
            'single-long-side-edge': '08',
            'single-short-side-edge': '09',
            'single-short-side-inset': '10',
            'round-full-frame-edge': '11',
            'round-no-frost': '12'
        };
        $('#product-configurator select, #product-configurator input').each(function() {
            if (this.id === 'style') {
                sku += styleCodes[$(this).val()];
            } else {
                sku += $(this).val();
            }
        });
        $('#product-details').text('SKU: ' + sku);
    }
    >>>>>>> UPDATED
    ```
    
    Please confirm if this meets your requirements. After that, we can proceed with writing the functions to update the product image, update the selected options, and apply the conditional logic.

--- new_file.js
+++ new_file.js
@@ -17,7 +17,16 @@ function generateSKU() {
     var styleCodes = {
         'full-frame-inset': '01',
         'full-frame-edge': '02',
-        // Add the rest of the style codes here
+        'double-long-side-inset': '03',
+        'double-long-side-edge': '04',
+        'no-frost': '05',
+        'double-short-side-edge': '06',
+        'double-short-side-inset': '07',
+        'single-long-side-edge': '08',
+        'single-short-side-edge': '09',
+        'single-short-side-inset': '10',
+        'round-full-frame-edge': '11',
+        'round-no-frost': '12'
     };
     $('#product-configurator select, #product-configurator input').each(function() {
         if (this.id === 'style') {
